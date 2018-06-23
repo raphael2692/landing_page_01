54a33195e8220b79182cdfc0673af27e7873bb86
raffaele2692 <raffaele2692@gmail.com>
2018-06-23 11:53:39 +0200

    new animations

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,19 @@
+$( document ).ready(function() {
+    
+
+
 $('.myButton').hover(
     function(){ $(this).addClass('animated pulse infinite') },
     function(){ $(this).removeClass('animated pulse infinite') }
 )
 
+$('.nav-item').hover(
+    function () {
+        $(this).addClass('animated pulse infinite')
+    },
+    function () {
+        $(this).removeClass('animated pulse infinite')
+    }
+)
+
+});
